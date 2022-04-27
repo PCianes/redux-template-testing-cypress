1cd12f54435ffbc971fbc2b464e812bebafe1949
Pablo Cianes <cianespablo@gmail.com>
2022-04-27 12:05:30 +0200

    🎨 add styled components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import styled from 'styled-components'
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
+  const Title = styled.h1`
+    background-color: rgb(112, 76, 182, .9);
+    color: #fff;
+    padding: 5px 15px;
+    border-radius: 10px;
+  `
+
   return (
     <div className="App">
       <header className="App-header">
+        <Title>Redux template: Jest & Cypress</Title>
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
